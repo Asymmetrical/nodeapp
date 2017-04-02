3cdab2d5f5f3c152a2aaca36b33cc65e937d5949
JohanW <Johan.Wastring@gmail.com>
2017-04-02 21:44:01 +0200

    NodeJS base API app complete

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,26 @@
 // routes/note_routes.js
 
+//since _id is an object, not a string 
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+
+
+app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    // without the below it will not work, since _id is an object, 
+    // not a string 
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  }); //get END
+
+
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
@@ -10,5 +30,35 @@ module.exports = function(app, db) {
         res.send(result.ops[0]);
       }
     });
-  });
-};
+  }); //post END
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  }); //Delete END
+
+// Essentially Update
+// So getting it and changing it (in the calls 'Body', i.e. not 'Header')
+// The 'Body' contains title and Body
+// Getting and changing in one swoop
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
+  }); // Put END
+
+}; //module.exports
